184e75a72eaec3d0d31f07e290fa5461732485be
Wouter Zomervrucht <149009213+wzomervrucht@users.noreply.github.com>
2023-12-17 21:12:19 +0100

    simple runner

---
+++ puzzles/01/trebuchet.js
@@ -0,0 +1,12 @@
+"use strict";
+
+function solve1(input) {}
+
+function solve2(input) {}
+
+module.exports = {
+  day: 1,
+  title: "Trebuchet?!",
+  solve1: solve1,
+  solve2: solve2
+};
--- run.js
+++ run.js
@@ -1,3 +1,13 @@
 "use strict";
 
-console.log("Advent of Code 2023");
+var fs = require("fs");
+
+function readFile(file) {
+  return fs.readFileSync(file, { encoding: "utf8" });
+}
+
+var puzzle = require("./puzzles/01/trebuchet.js");
+var input = readFile("./puzzles/01/trebuchet.txt");
+
+var answer = puzzle.solve1(input);
+console.log(answer);
