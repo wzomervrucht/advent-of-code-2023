94a07720250681c3bee54af27cbcf2d5cf221888
Wouter Zomervrucht <149009213+wzomervrucht@users.noreply.github.com>
2023-12-22 17:22:16 +0100

    input preprocessing

--- run.js
+++ run.js
@@ -1,13 +1,21 @@
 "use strict";
 
 var fs = require("fs");
+var assert = require("./puzzles/common/assert");
 
-function readFile(file) {
-  return fs.readFileSync(file, { encoding: "utf8" });
+function getInput(file) {
+  var input = fs.readFileSync(file, { encoding: "utf8" });
+  assert(typeof input === "string", "Input should be a string.");
+  assert(input.indexOf("\r") === -1, "Input should have LF line endings.");
+  assert(input[input.length - 1] === "\n", "Input should have a final newline.");
+  return input.slice(0, -1).split("\n");
 }
 
-var puzzle = require("./puzzles/01/trebuchet.js");
-var input = readFile("./puzzles/01/trebuchet.txt");
-
-var answer = puzzle.solve1(input);
-console.log(answer);
+try {
+  var puzzle = require("./puzzles/01/trebuchet.js");
+  var input = getInput("./puzzles/01/trebuchet.txt");
+  var answer = puzzle.solve1(input);
+  console.log(answer);
+} catch (error) {
+  console.log(error.message);
+}
